22de21441664e83cbfb4a2aa82a81217bdcf3b5e
Parv Rehan <z5421600@ad.unsw.edu.au>
2024-05-21 13:36:20 +1000

    Added /register route in App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import Register from './pages/Register';
+
 import './index.css';
 
 function App() {
@@ -8,6 +10,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<></>} />
+        <Route path='/register' element={<Register />} />
       </Routes>
     </BrowserRouter>
   );

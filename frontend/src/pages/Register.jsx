6694ba4055f475345e2fb3889b8353cc7390e70f
Parv Rehan <z5421600@ad.unsw.edu.au>
2024-05-21 13:38:19 +1000

    Skeleton template for Register Page

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import axios from 'axios';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
+import { TextInput, Button, Label } from "flowbite-react";
+import { HiUser, HiMail, HiLockClosed } from 'react-icons/hi';
+
+const Register  = () => {
+  const navigate = useNavigate();
+
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [confirmedPassword, setConfirmedPassword] = React.useState('');
+  const [name, setName] = React.useState('')
+
+  const [errors, setErrors] = React.useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    // Password Validation
+    const registerBody = { email, password, name };
+
+    try {
+      const resp = await axios.post('http://localhost:5000/auth/register', registerBody);      
+      navigate('/dashboard');
+    } catch (error) {
+      setErrors('An error occurred while registering.');
+    }
+
+    return;
+  }
+
+  return (
+    <div className='flex flex-col justify-center items-center h-screen'>
+      <form 
+        className='flex flex-col gap-6 w-full max-w-xl px-4 py-8'
+        onSubmit={handleSubmit}
+      >
+      <div>
+          <Label value='Name' />
+          <TextInput
+            id='register-name'
+            type='text'
+            icon={HiUser}
+            onChange={(e) => setName(e.target.value)}
+            placeholder='parvyyy'
+            required
+          />
+        </div>
+        <div>
+          <Label value='Email' />
+          <TextInput
+            id='register-email'
+            type='email'
+            icon={HiMail}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder='parvyyy@email.com'
+            required
+          />
+        </div>
+        <div>
+          <Label value='Password' />
+          <TextInput
+            id='register-password'
+            type='password'
+            icon={HiLockClosed}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder='******'
+            required
+          />
+        </div>
+        <div>
+          <Label value='Confirm Password' />
+          <TextInput
+            id='register-confirm-password'
+            type='password'
+            icon={HiLockClosed}
+            onChange={(e) => setConfirmedPassword(e.target.value)}
+            placeholder='******'
+            required
+          />
+        </div>
+        <Button 
+          id='register-submit' 
+          className='bg-violet-600 enabled:hover:bg-violet-700' 
+          type='submit'
+        >
+          Register
+        </Button>
+        <Label>
+          Already have an account?{' '}
+          <Link to='/login' className='text-violet-600 hover:underline'>
+            Log-in
+          </Link>
+        </Label>
+      </form>
+    </div>
+  )
+}
+
+export default Register;
